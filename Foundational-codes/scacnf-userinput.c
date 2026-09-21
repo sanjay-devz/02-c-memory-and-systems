22aed1298da490defa6cd445675efa9e7c7cae88
sanjay-devz <sanjay.gridz@gmail.com>
2026-09-22 04:22:44 +0530

    file name changed

---
+++ Foundational-codes/scacnf-userinput.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    int a, b, c, d, e;
+    printf("Enter First Integer: ");
+    scanf("%d",&a); 
+    printf("Enter Second Integer: ");
+    scanf("%d",&b);
+
+    c=a-b;
+
+    printf("the sum is %d", c);
+    printf("The product of %d", d)
+}
